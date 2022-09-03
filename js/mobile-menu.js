a95a25bd37be0216f40da354b8018e10db8c22df
Anastasia-Wednesday <anastasia.wednesday@gmail.com>
2022-09-03 18:26:38 +0300

    mobile

---
+++ js/mobile-menu.js
@@ -0,0 +1,37 @@
+// (() => {
+//   const menuContainer = document.querySelector('.js-menu-container');
+//   const openMenuBtn = document.querySelector('.js-open-menu');
+//   const closeMenuBtn = document.querySelector('.js-close-menu');
+
+//   const toggleMenu = () => menuContainer.classList.toggle('is-open');
+//   openMenuBtn.addEventListener('click', toggleMenu);
+//   closeMenuBtn.addEventListener('click', toggleMenu);
+
+//   // Закрываем мобильное меню на более широких экранах
+//   // в случае изменения ориентации устройства.
+//   window
+//     .matchMedia('(min-width: 600px)')
+//     .addEventListener('change', e => e.matches && menuContainer.classList.remove('is-open'));
+// })();
+
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
+    body: document.querySelector('body'),
+    menuList: document.querySelector('.mobile-menu'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
+
+  function removeMenu() {
+    refs.menu.classList.add('is-hidden');
+  }
+})();
